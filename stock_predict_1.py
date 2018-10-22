da66a3eab6ea0c736afff350caf8f6f8014a1459
yangwohenmai <zongxianyang@outlook.com>
2018-10-22 21:24:03 +0800

    first time submit

---
+++ stock_predict_1.py
@@ -0,0 +1,121 @@
+# coding=utf-8
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+#——————————————————导入数据——————————————————————
+f = open('C:\\Users\\Administrator\\Desktop\\dataset_1.csv')
+df = pd.read_csv(f)         #读入股票数据
+data = np.array(df['max'])  #获取最高价序列
+data = data[::-1]#反转，使数据按照日期先后顺序排列
+#以折线图展示data
+# plt.figure()
+# plt.plot(data)
+# plt.show()
+normalize_data = (data - np.mean(data)) / np.std(data)#标准化
+normalize_data = normalize_data[:, np.newaxis]#增加维度
+
+#———————————————————形成训练集—————————————————————
+time_step = 20      #时间步
+rnn_unit = 10       #hidden layer units
+lstm_layers = 2     #每一批次训练多少个样例
+batch_size = 60     #输入层维度  #每一批次训练多少个样例
+input_size = 1      #输入层维度
+output_size = 1     #输出层维度
+lr = 0.0006         #学习率
+train_x, train_y = [], []#训练集
+for i in range(len(normalize_data) - time_step - 1):
+    x = normalize_data[i:i + time_step]
+    y = normalize_data[i + 1:i + time_step + 1]
+    train_x.append(x.tolist())
+    train_y.append(y.tolist())
+
+X = tf.placeholder(tf.float32, [None, time_step, input_size])   #每批次输入网络的tensor
+Y = tf.placeholder(tf.float32, [None, time_step, output_size])  #每批次tensor对应的标签
+#——————————————————定义神经网络变量——————————————————
+#输入层、输出层权重、偏置
+weights = {
+    'in': tf.Variable(tf.random_normal([input_size, rnn_unit])),
+    'out': tf.Variable(tf.random_normal([rnn_unit, 1]))
+}
+biases = {
+    'in': tf.Variable(tf.constant(0.1, shape=[rnn_unit, ])),
+    'out': tf.Variable(tf.constant(0.1, shape=[1, ]))
+}
+
+#参数：输入网络批次数目
+def lstm(batch):#参数：输入网络批次数目
+    w_in = weights['in']
+    b_in = biases['in']
+    input = tf.reshape(X, [-1, input_size])#需要将tensor转成2维进行计算，计算后的结果作为隐藏层的输入
+    input_rnn = tf.matmul(input, w_in) + b_in
+    input_rnn = tf.reshape(input_rnn, [-1, time_step, rnn_unit])#将tensor转成3维，作为lstm cell的输入
+    cell = tf.nn.rnn_cell.MultiRNNCell([tf.nn.rnn_cell.BasicLSTMCell(rnn_unit) for i in range(lstm_layers)])
+    init_state = cell.zero_state(batch, dtype=tf.float32)
+    output_rnn, final_states = tf.nn.dynamic_rnn(cell, input_rnn, initial_state=init_state, dtype=tf.float32)
+    output = tf.reshape(output_rnn, [-1, rnn_unit])#作为输出层的输入
+    w_out = weights['out']
+    b_out = biases['out']
+    pred = tf.matmul(output, w_out) + b_out
+    return pred, final_states
+
+
+def train_lstm():
+    global batch_size
+    with tf.variable_scope("sec_lstm"):
+        pred, _ = lstm(batch_size)
+    # 损失函数
+    loss = tf.reduce_mean(tf.square(tf.reshape(pred, [-1]) - tf.reshape(Y, [-1])))
+    train_op = tf.train.AdamOptimizer(lr).minimize(loss)
+    saver = tf.train.Saver(tf.global_variables())
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        # 重复训练10000次
+        for i in range(100):  # We can increase the number of iterations to gain better result.
+            step = 0
+            start = 0
+            end = start + batch_size
+            print("i = ",i)
+            while (end < len(train_x)):
+                _, loss_ = sess.run([train_op, loss], feed_dict={X: train_x[start:end], Y: train_y[start:end]})
+                start += batch_size
+                end = start + batch_size
+
+                if step % 100 == 0:  #每10步保存一次参数
+                    print("Number of iterations:", i, " loss:", loss_)
+                    print("model_save", saver.save(sess, 'model_save1\\modle.ckpt'))
+                    # I run the code in windows 10,so use  'model_save1\\modle.ckpt'
+                    # if you run it in Linux,please use  'model_save1/modle.ckpt'
+                step += 1
+        print("The train has finished")
+
+
+#train_lstm()
+
+
+def prediction():
+    with tf.variable_scope("sec_lstm", reuse=tf.AUTO_REUSE):
+        pred, _ = lstm(1)#预测时只输入[1,time_step,input_size]的测试数据
+    saver = tf.train.Saver(tf.global_variables())
+    with tf.Session() as sess:
+        saver.restore(sess, 'model_save1\\modle.ckpt')#参数恢复
+        # I run the code in windows 10,so use  'model_save1\\modle.ckpt'
+        # if you run it in Linux,please use  'model_save1/modle.ckpt'
+        # 取训练集最后一行为测试样本。shape=[1,time_step,input_size]
+        prev_seq = train_x[-1]
+        predict = []
+        # 得到之后100个预测结果
+        for i in range(100):
+            next_seq = sess.run(pred, feed_dict={X: [prev_seq]})
+            predict.append(next_seq[-1])
+            # 每次得到最后一个时间步的预测结果，与之前的数据加在一起，形成新的测试样本
+            prev_seq = np.vstack((prev_seq[1:], next_seq[-1]))
+        # 以折线图表示结果
+        plt.figure()
+        plt.plot(list(range(len(normalize_data))), normalize_data, color='b')
+        plt.plot(list(range(len(normalize_data), len(normalize_data) + len(predict))), predict, color='r')
+        plt.show()
+
+
+prediction()
---
+++ stock_predict_2.py
@@ -0,0 +1,151 @@
+# coding=utf-8
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+rnn_unit = 10  # 隐层神经元的个数
+lstm_layers = 2  # 隐层层数
+input_size = 7
+output_size = 1
+lr = 0.0006  # 学习率
+# ——————————————————导入数据——————————————————————
+f = open('C:\\Users\\Administrator\\Desktop\\dataset_2.csv')
+df = pd.read_csv(f)  # 读入股票数据
+data = df.iloc[:, 2:10].values  # 取第3-10列
+
+
+# 获取训练集
+def get_train_data(batch_size=60, time_step=20, train_begin=0, train_end=5800):
+    batch_index = []
+    data_train = data[train_begin:train_end]
+    normalized_train_data = (data_train - np.mean(data_train, axis=0)) / np.std(data_train, axis=0)  # 标准化
+    train_x, train_y = [], []  # 训练集x和y初定义
+    for i in range(len(normalized_train_data) - time_step):
+        if i % batch_size == 0:
+            batch_index.append(i)
+        x = normalized_train_data[i:i + time_step, :7]
+        y = normalized_train_data[i:i + time_step, 7, np.newaxis]
+        train_x.append(x.tolist())
+        train_y.append(y.tolist())
+    batch_index.append((len(normalized_train_data) - time_step))
+    return batch_index, train_x, train_y
+
+
+# 获取测试集
+def get_test_data(time_step=20, test_begin=5800):
+    data_test = data[test_begin:]
+    mean = np.mean(data_test, axis=0)
+    std = np.std(data_test, axis=0)
+    normalized_test_data = (data_test - mean) / std  # 标准化
+    size = (len(normalized_test_data) + time_step - 1) // time_step  # 有size个sample
+    test_x, test_y = [], []
+    for i in range(size - 1):
+        x = normalized_test_data[i * time_step:(i + 1) * time_step, :7]
+        y = normalized_test_data[i * time_step:(i + 1) * time_step, 7]
+        test_x.append(x.tolist())
+        test_y.extend(y)
+    test_x.append((normalized_test_data[(i + 1) * time_step:, :7]).tolist())
+    test_y.extend((normalized_test_data[(i + 1) * time_step:, 7]).tolist())
+    return mean, std, test_x, test_y
+
+
+# ——————————————————定义神经网络变量——————————————————
+# 输入层、输出层权重、偏置、dropout参数
+
+weights = {
+    'in': tf.Variable(tf.random_normal([input_size, rnn_unit])),
+    'out': tf.Variable(tf.random_normal([rnn_unit, 1]))
+}
+biases = {
+    'in': tf.Variable(tf.constant(0.1, shape=[rnn_unit, ])),
+    'out': tf.Variable(tf.constant(0.1, shape=[1, ]))
+}
+keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+
+
+# ——————————————————定义神经网络变量——————————————————
+def lstmCell():
+    # basicLstm单元
+    basicLstm = tf.nn.rnn_cell.BasicLSTMCell(rnn_unit)
+    # dropout
+    drop = tf.nn.rnn_cell.DropoutWrapper(basicLstm, output_keep_prob=keep_prob)
+    return basicLstm
+
+
+def lstm(X):
+    batch_size = tf.shape(X)[0]
+    time_step = tf.shape(X)[1]
+    w_in = weights['in']
+    b_in = biases['in']
+    input = tf.reshape(X, [-1, input_size])  # 需要将tensor转成2维进行计算，计算后的结果作为隐藏层的输入
+    input_rnn = tf.matmul(input, w_in) + b_in
+    input_rnn = tf.reshape(input_rnn, [-1, time_step, rnn_unit])  # 将tensor转成3维，作为lstm cell的输入
+    cell = tf.nn.rnn_cell.MultiRNNCell([lstmCell() for i in range(lstm_layers)])
+    init_state = cell.zero_state(batch_size, dtype=tf.float32)
+    output_rnn, final_states = tf.nn.dynamic_rnn(cell, input_rnn, initial_state=init_state, dtype=tf.float32)
+    output = tf.reshape(output_rnn, [-1, rnn_unit])
+    w_out = weights['out']
+    b_out = biases['out']
+    pred = tf.matmul(output, w_out) + b_out
+    return pred, final_states
+
+
+# ————————————————训练模型————————————————————
+
+def train_lstm(batch_size=60, time_step=20, train_begin=2000, train_end=5800):
+    X = tf.placeholder(tf.float32, shape=[None, time_step, input_size])
+    Y = tf.placeholder(tf.float32, shape=[None, time_step, output_size])
+    batch_index, train_x, train_y = get_train_data(batch_size, time_step, train_begin, train_end)
+    with tf.variable_scope("sec_lstm"):
+        pred, _ = lstm(X)
+    loss = tf.reduce_mean(tf.square(tf.reshape(pred, [-1]) - tf.reshape(Y, [-1])))
+    train_op = tf.train.AdamOptimizer(lr).minimize(loss)
+    saver = tf.train.Saver(tf.global_variables(), max_to_keep=15)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for i in range(10):  # 这个迭代次数，可以更改，越大预测效果会更好，但需要更长时间
+            for step in range(len(batch_index) - 1):
+                _, loss_ = sess.run([train_op, loss], feed_dict={X: train_x[batch_index[step]:batch_index[step + 1]],
+                                                                 Y: train_y[batch_index[step]:batch_index[step + 1]],
+                                                                 keep_prob: 0.5})
+            print("Number of iterations:", i, " loss:", loss_)
+        print("model_save: ", saver.save(sess, 'model_save2\\modle.ckpt'))
+        # I run the code on windows 10,so use  'model_save2\\modle.ckpt'
+        # if you run it on Linux,please use  'model_save2/modle.ckpt'
+        print("The train has finished")
+
+
+train_lstm()
+
+
+# ————————————————预测模型————————————————————
+def prediction(time_step=20):
+    X = tf.placeholder(tf.float32, shape=[None, time_step, input_size])
+    mean, std, test_x, test_y = get_test_data(time_step)
+    with tf.variable_scope("sec_lstm", reuse=tf.AUTO_REUSE):
+        pred, _ = lstm(X)
+    saver = tf.train.Saver(tf.global_variables())
+    with tf.Session() as sess:
+        # 参数恢复
+        module_file = tf.train.latest_checkpoint('model_save2')
+        saver.restore(sess, module_file)
+        test_predict = []
+        for step in range(len(test_x) - 1):
+            prob = sess.run(pred, feed_dict={X: [test_x[step]], keep_prob: 1})
+            predict = prob.reshape((-1))
+            test_predict.extend(predict)
+        test_y = np.array(test_y) * std[7] + mean[7]
+        test_predict = np.array(test_predict) * std[7] + mean[7]
+        acc = np.average(np.abs(test_predict - test_y[:len(test_predict)]) / test_y[:len(test_predict)])  # 偏差程度
+        print("The accuracy of this predict:", acc)
+        # 以折线图表示结果
+        plt.figure()
+        plt.plot(list(range(len(test_predict))), test_predict, color='b', )
+        plt.plot(list(range(len(test_y))), test_y, color='r')
+        plt.show()
+
+
+prediction()
