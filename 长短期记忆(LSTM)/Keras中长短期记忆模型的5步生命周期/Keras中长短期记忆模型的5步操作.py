26da6fa40272023ad7d65f90b25edc763e05cf7f
yangwohenmai <zongxianyang@outlook.com>
2018-12-03 16:32:55 +0800

    update

--- 长短期记忆(LSTM)/Keras中长短期记忆模型的5步生命周期/Keras中长短期记忆模型的5步操作.py
+++ 长短期记忆(LSTM)/Keras中长短期记忆模型的5步生命周期/Keras中长短期记忆模型的5步操作.py
@@ -1,32 +1,31 @@
-# Example of LSTM to learn a sequence
 from pandas import DataFrame
 from pandas import concat
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
-# create sequence
+# 创建一个0.1~0.9的序列
 length = 10
 sequence = [i/float(length) for i in range(length)]
 print(sequence)
-# create X/y pairs
+# 构建一个X->y的映射关系
 df = DataFrame(sequence)
 df = concat([df.shift(1), df], axis=1)
 df.dropna(inplace=True)
-# convert to LSTM friendly format
+# 使用reshape方法，把序列转换为LSTM可识别的数组格式
 values = df.values
 X, y = values[:, 0], values[:, 1]
 X = X.reshape(len(X), 1, 1)
-# 1. define network
+# 1. 定义网络类型
 model = Sequential()
 model.add(LSTM(10, input_shape=(1,1)))
 model.add(Dense(1))
-# 2. compile network
+# 2. 编译网络，设置损失参数
 model.compile(optimizer='adam', loss='mean_squared_error')
-# 3. fit network
+# 3. 调用网络开始训练模型
 history = model.fit(X, y, epochs=1000, batch_size=len(X), verbose=0)
-# 4. evaluate network
+# 4. 评估网络
 loss = model.evaluate(X, y, verbose=0)
 print(loss)
-# 5. make predictions
+# 5. 利用训练好的模型，带入原始的X进行单步预测
 predictions = model.predict(X, verbose=0)
 print(predictions[:, 0])
