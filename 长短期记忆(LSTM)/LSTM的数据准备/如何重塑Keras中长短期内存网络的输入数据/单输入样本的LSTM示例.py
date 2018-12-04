83c05bb1a460c83d4ab761069f7ba5938fa0343c
yangwohenmai <zongxianyang@outlook.com>
2018-12-04 12:26:00 +0800

    update

---
+++ 长短期记忆(LSTM)/LSTM的数据准备/如何重塑Keras中长短期内存网络的输入数据/单输入样本的LSTM示例.py
@@ -0,0 +1,7 @@
+from numpy import array
+# 将这个数字序列定义为NumPy数组
+data = array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+# 我们可以使用NumPy数组上的reshape（）函数将这个一维数组重新整形为一个三维数组，每个时间步长有1个样本，10个时间步长和1个特征(列)
+data = data.reshape((1, 10, 1))
+print(data.shape)
+print(data)
