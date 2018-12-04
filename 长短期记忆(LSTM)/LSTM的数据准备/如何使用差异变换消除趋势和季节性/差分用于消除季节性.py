c735a869b11b5a1cf507403f42d3f2e27124330c
yangwohenmai <zongxianyang@outlook.com>
2018-12-04 16:29:00 +0800

    update

--- 长短期记忆(LSTM)/LSTM的数据准备/如何使用差异变换消除趋势和季节性/差分用于消除季节性.py
+++ 长短期记忆(LSTM)/LSTM的数据准备/如何使用差异变换消除趋势和季节性/差分用于消除季节性.py
@@ -2,7 +2,7 @@ from math import sin
 from math import radians
 from matplotlib import pyplot
 
-# create a differenced series
+# 创建一个差分序列
 def difference(dataset, interval=1):
 	diff = list()
 	for i in range(interval, len(dataset)):
@@ -10,19 +10,19 @@ def difference(dataset, interval=1):
 		diff.append(value)
 	return diff
 
-# invert differenced forecast
+# 差分的逆转换
 def inverse_difference(last_ob, value):
 	return value + last_ob
 
-# define a dataset with a linear trend
+# 定义一个有线性趋势的数据集
 data = [sin(radians(i)) for i in range(360)] + [sin(radians(i)) for i in range(360)]
 pyplot.plot(data)
 pyplot.show()
-# difference the dataset
+# 对数据做差分
 diff = difference(data, 360)
 pyplot.plot(diff)
 pyplot.show()
-# invert the difference
+# 差分的逆转换
 inverted = [inverse_difference(data[i], diff[i]) for i in range(len(diff))]
 pyplot.plot(inverted)
 pyplot.show()
--- 长短期记忆(LSTM)/LSTM的数据准备/如何使用差异变换消除趋势和季节性/差分用于消除趋势.py
+++ 长短期记忆(LSTM)/LSTM的数据准备/如何使用差异变换消除趋势和季节性/差分用于消除趋势.py
@@ -1,4 +1,4 @@
-# create a differenced series
+# 创建一个差分序列
 def difference(dataset, interval=1):
 	diff = list()
 	for i in range(interval, len(dataset)):
@@ -6,16 +6,16 @@ def difference(dataset, interval=1):
 		diff.append(value)
 	return diff
 
-# invert differenced forecast
+# 差分的逆转换
 def inverse_difference(last_ob, value):
 	return value + last_ob
 
-# define a dataset with a linear trend
+# 定义一个有线性趋势的数据集
 data = [i+1 for i in range(20)]
 print(data)
-# difference the dataset
+# 对数据做差分
 diff = difference(data)
 print(diff)
-# invert the difference
+# 差分的逆转换
 inverted = [inverse_difference(data[i], diff[i]) for i in range(len(diff))]
 print(inverted)
