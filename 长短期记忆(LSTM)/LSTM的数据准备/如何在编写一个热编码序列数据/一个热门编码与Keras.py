e3996e99b1c5783ed9ed352e4966f3a84a26df09
yangwohenmai <zongxianyang@outlook.com>
2018-12-04 15:20:51 +0800

    update

--- 长短期记忆(LSTM)/LSTM的数据准备/如何在编写一个热编码序列数据/一个热门编码与Keras.py
+++ 长短期记忆(LSTM)/LSTM的数据准备/如何在编写一个热编码序列数据/一个热门编码与Keras.py
@@ -1,13 +1,13 @@
 from numpy import array
 from numpy import argmax
 from keras.utils import to_categorical
-# define example
+# 定义一个实例
 data = [1, 3, 2, 0, 3, 2, 2, 1, 0, 1]
 data = array(data)
 print(data)
-# one hot encode
+# 编码
 encoded = to_categorical(data)
 print(encoded)
-# invert encoding
+# 逆转换第一行
 inverted = argmax(encoded[0])
 print(inverted)
--- 长短期记忆(LSTM)/LSTM的数据准备/如何在编写一个热编码序列数据/手动一个热编码.py
+++ 长短期记忆(LSTM)/LSTM的数据准备/如何在编写一个热编码序列数据/手动一个热编码.py
@@ -1,21 +1,21 @@
 from numpy import argmax
-# define input string
+# 定义一个编码字符串
 data = 'hello world'
 print(data)
-# define universe of possible input values
+# 定义输入值得所有分类类型
 alphabet = 'abcdefghijklmnopqrstuvwxyz '
 # define a mapping of chars to integers
 char_to_int = dict((c, i) for i, c in enumerate(alphabet))
 int_to_char = dict((i, c) for i, c in enumerate(alphabet))
-# integer encode input data
+# 转换成整形编码输入
 integer_encoded = [char_to_int[char] for char in data]
 print(integer_encoded)
-# one hot encode
+# 编码过程
 onehot_encoded = list()
 for value in integer_encoded:
 	letter = [0 for _ in range(len(alphabet))]
 	letter[value] = 1
 	onehot_encoded.append(letter)
 print(onehot_encoded)
-# invert encoding
+# 逆转换第一行数据
 inverted = int_to_char[argmax(onehot_encoded[0])]
