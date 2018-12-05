6c5febe5ba835e842ed3eb857f08e8bc2ea3c590
yangwohenmai <zongxianyang@outlook.com>
2018-12-05 15:37:55 +0800

    update

--- 长短期记忆(LSTM)/LSTM的数据准备/如何处理序列预测中的缺失时间步长/替换缺失的序列数据.py
+++ 长短期记忆(LSTM)/LSTM的数据准备/如何处理序列预测中的缺失时间步长/替换缺失的序列数据.py
@@ -3,26 +3,26 @@ from numpy import array
 from pandas import concat
 from pandas import DataFrame
 
-# generate a sequence of random values
+# 生成一系列随机值
 def generate_sequence(n_timesteps):
 	return [random() for _ in range(n_timesteps)]
 
-# generate data for the lstm
+# 生成lstm的数据
 def generate_data(n_timesteps):
 	# generate sequence
 	sequence = generate_sequence(n_timesteps)
 	sequence = array(sequence)
-	# create lag
+	# 格式化成有序矩阵
 	df = DataFrame(sequence)
 	df = concat([df.shift(1), df], axis=1)
-	# replace missing values with -1
+	# 把NAN值用-1来填充
 	df.fillna(-1, inplace=True)
 	values = df.values
-	# specify input and output data
+	# 指定输入和输出数据
 	X, y = values, values[:, 1]
 	return X, y
 
-# generate sequence
+# 生成序列
 n_timesteps = 10
 X, y = generate_data(n_timesteps)
 # print sequence
