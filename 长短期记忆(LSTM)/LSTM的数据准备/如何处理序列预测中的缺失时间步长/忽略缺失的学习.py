e4bab6d1d0ffc447dc94a097f4c6a3acf2e9869a
yangwohenmai <zongxianyang@outlook.com>
2018-12-05 15:12:27 +0800

    update

---
+++ 长短期记忆(LSTM)/LSTM的数据准备/如何处理序列预测中的缺失时间步长/忽略缺失的学习.py
@@ -0,0 +1,47 @@
+from random import random
+from numpy import array
+from pandas import concat
+from pandas import DataFrame
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Masking
+
+# generate a sequence of random values
+def generate_sequence(n_timesteps):
+	return [random() for _ in range(n_timesteps)]
+
+# generate data for the lstm
+def generate_data(n_timesteps):
+	# generate sequence
+	sequence = generate_sequence(n_timesteps)
+	sequence = array(sequence)
+	# create lag
+	df = DataFrame(sequence)
+	df = concat([df.shift(1), df], axis=1)
+	# replace missing values with -1
+	df.fillna(-1, inplace=True)
+	values = df.values
+	# specify input and output data
+	X, y = values, values[:, 1]
+	# reshape
+	X = X.reshape(len(X), 2, 1)
+	y = y.reshape(len(y), 1)
+	return X, y
+
+n_timesteps = 10
+# define model
+model = Sequential()
+model.add(Masking(mask_value=-1, input_shape=(2, 1)))
+model.add(LSTM(5))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+# fit model
+for i in range(500):
+	X, y = generate_data(n_timesteps)
+	model.fit(X, y, epochs=1, batch_size=1, verbose=2)
+# evaluate model on new data
+X, y = generate_data(n_timesteps)
+yhat = model.predict(X)
+for i in range(len(X)):
+	print('Expected', y[i,0], 'Predicted', yhat[i,0])
