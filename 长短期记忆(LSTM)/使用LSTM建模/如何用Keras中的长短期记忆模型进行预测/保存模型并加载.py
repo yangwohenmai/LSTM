ece64ba2a18faa0aba75a3ca07df6c25282b6840
yangwohenmai <zongxianyang@outlook.com>
2018-12-06 23:34:28 +0800

    update LSTM

---
+++ 长短期记忆(LSTM)/使用LSTM建模/如何用Keras中的长短期记忆模型进行预测/保存模型并加载.py
@@ -0,0 +1,34 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from numpy import array
+from keras.models import load_model
+
+# return training data
+def get_train():
+    seq = [[0.0, 0.1], [0.1, 0.2], [0.2, 0.3], [0.3, 0.4], [0.4, 0.5]]
+    seq = array(seq)
+    X, y = seq[:, 0], seq[:, 1]
+    X = X.reshape((len(X), 1, 1))
+    return X, y
+
+# define model
+model = Sequential()
+model.add(LSTM(10, input_shape=(1,1)))
+model.add(Dense(1, activation='linear'))
+# compile model
+model.compile(loss='mse', optimizer='adam')
+# fit model
+X,y = get_train()
+model.fit(X, y, epochs=300, shuffle=False, verbose=0)
+# save model to single file
+model.save('lstm_model.h5')
+
+# snip...
+# later, perhaps run from another script
+
+# load model from single file
+model = load_model('lstm_model.h5')
+# make predictions
+yhat = model.predict(X, verbose=0)
+print(yhat)
