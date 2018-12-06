b21276b1bdee61ea497dc6f17f8f03e10e94c9d7
yangwohenmai <zongxianyang@outlook.com>
2018-12-06 15:28:28 +0800

    update

---
+++ 长短期记忆(LSTM)/使用LSTM建模/堆叠的长短期记忆网络/在Keras中实现堆叠LSTM 2D输出.py
@@ -0,0 +1,13 @@
+# Example of one output for whole sequence
+from keras.models import Sequential
+from keras.layers import LSTM
+from numpy import array
+# define model where LSTM is also output layer
+model = Sequential()
+model.add(LSTM(1, input_shape=(3,1)))
+model.compile(optimizer='adam', loss='mse')
+# input time steps
+data = array([0.1, 0.2, 0.3]).reshape((1,3,1))
+# make and show prediction
+print(model.predict(data))
+
---
+++ 长短期记忆(LSTM)/使用LSTM建模/堆叠的长短期记忆网络/在Keras中实现堆叠LSTM 3D输出.py
@@ -0,0 +1,12 @@
+# Example of one output for each input time step
+from keras.models import Sequential
+from keras.layers import LSTM
+from numpy import array
+# define model where LSTM is also output layer
+model = Sequential()
+model.add(LSTM(1, return_sequences=True, input_shape=(3,1)))
+model.compile(optimizer='adam', loss='mse')
+# input time steps
+data = array([0.1, 0.2, 0.3]).reshape((1,3,1))
+# make and show prediction
+print(model.predict(data))
