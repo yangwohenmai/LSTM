a22d2ef827bb851e5b13cedd4c05213d6f7c2c8e
yangwohenmai <zongxianyang@outlook.com>
2018-12-06 17:44:37 +0800

    update

---
+++ 长短期记忆(LSTM)/使用LSTM建模/如何诊断LSTM模型的过度拟合和欠拟合/不合格的例子/训练周期不足.py
@@ -0,0 +1,40 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from matplotlib import pyplot
+from numpy import array
+
+# return training data
+def get_train():
+	seq = [[0.0, 0.1], [0.1, 0.2], [0.2, 0.3], [0.3, 0.4], [0.4, 0.5]]
+	seq = array(seq)
+	X, y = seq[:, 0], seq[:, 1]
+	X = X.reshape((len(X), 1, 1))
+	return X, y
+
+# return validation data
+def get_val():
+	seq = [[0.5, 0.6], [0.6, 0.7], [0.7, 0.8], [0.8, 0.9], [0.9, 1.0]]
+	seq = array(seq)
+	X, y = seq[:, 0], seq[:, 1]
+	X = X.reshape((len(X), 1, 1))
+	return X, y
+
+# define model
+model = Sequential()
+model.add(LSTM(10, input_shape=(1,1)))
+model.add(Dense(1, activation='linear'))
+# compile model
+model.compile(loss='mse', optimizer='adam')
+# fit model
+X,y = get_train()
+valX, valY = get_val()
+history = model.fit(X, y, epochs=100, validation_data=(valX, valY), shuffle=False)
+# plot train and validation loss
+pyplot.plot(history.history['loss'])
+pyplot.plot(history.history['val_loss'])
+pyplot.title('model train vs validation loss')
+pyplot.ylabel('loss')
+pyplot.xlabel('epoch')
+pyplot.legend(['train', 'validation'], loc='upper right')
+pyplot.show()
