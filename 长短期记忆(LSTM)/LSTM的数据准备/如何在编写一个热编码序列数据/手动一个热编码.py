48bf4473b131b3dfc2249e638a03bf5b683eab9b
yangwohenmai <zongxianyang@outlook.com>
2018-12-10 10:22:44 +0800

    update

--- 长短期记忆(LSTM)/LSTM的数据准备/如何在编写一个热编码序列数据/手动一个热编码.py
+++ 长短期记忆(LSTM)/LSTM的数据准备/如何在编写一个热编码序列数据/手动一个热编码.py
@@ -10,6 +10,7 @@ int_to_char = dict((i, c) for i, c in enumerate(alphabet))
 # 转换成整形编码输入
 integer_encoded = [char_to_int[char] for char in data]
 print(integer_encoded)
+
 # 编码过程
 onehot_encoded = list()
 for value in integer_encoded:
@@ -17,5 +18,24 @@ for value in integer_encoded:
 	letter[value] = 1
 	onehot_encoded.append(letter)
 print(onehot_encoded)
+# 打印输出
+for vector in onehot_encoded:
+    print(vector)
+
 # 逆转换第一行数据
 inverted = int_to_char[argmax(onehot_encoded[0])]
+
+# 热编码封装方法
+def encode(pattern):
+    encoded = list()
+    for value in pattern:
+        row = [0.0 for x in range(len(pattern)-1)]
+        row[value] = 1.0
+        encoded.append(row)
+    return encoded
+
+seq1 = [3, 0, 1, 2, 3]
+encoded = encode(seq1)
+print(seq1)
+for vector in encoded:
+    print(vector)
