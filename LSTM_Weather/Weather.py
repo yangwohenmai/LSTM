abb5cc47ff1bc46032066011dec4699348b84ffd
yangwohenmai <zongxianyang@outlook.com>
2019-06-13 17:32:45 +0800

    LSTM五个步骤

--- LSTM_Weather/Weather.py
+++ LSTM_Weather/Weather.py
@@ -11,7 +11,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 
-# convert series to supervised learning
+# 格式化序列为监督型学习结构
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
     n_vars = 1 if type(data) is list else data.shape[1]
     df = DataFrame(data)
@@ -30,49 +30,49 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
     # put it all together
     agg = concat(cols, axis=1)
     agg.columns = names
-    # drop rows with NaN values
+    # 删除有NAN值的行
     if dropnan:
         agg.dropna(inplace=True)
     return agg
 
-# load dataset
+# 加载数据
 dataset = read_csv('pollution.csv', header=0, index_col=0)
 values = dataset.values
 # integer encode direction
 encoder = LabelEncoder()
 values[:,4] = encoder.fit_transform(values[:,4])
-# ensure all data is float
+# 数据转换成浮点型
 values = values.astype('float32')
 # normalize features
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled = scaler.fit_transform(values)
-# frame as supervised learning
+# 构建监督学习结构
 reframed = series_to_supervised(scaled, 1, 1)
-# drop columns we don't want to predict
+# 删除不需要预测的列
 reframed.drop(reframed.columns[[9,10,11,12,13,14,15]], axis=1, inplace=True)
 print(reframed.head())
 
-# split into train and test sets
+# 拆分训练集和测试集
 values = reframed.values
 n_train_hours = 365 * 24
 train = values[:n_train_hours, :]
 test = values[n_train_hours:, :]
-# split into input and outputs
+# 拆分输入和输出序列
 train_X, train_y = train[:, :-1], train[:, -1]
 test_X, test_y = test[:, :-1], test[:, -1]
-# reshape input to be 3D [samples, timesteps, features]
+# 构建一个 3D 的输入结构[samples, timesteps, features]
 train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
 test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
 print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
 
-# design network
+# 网络构建
 model = Sequential()
 model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
 model.add(Dense(1))
 model.compile(loss='mae', optimizer='adam')
-# fit network
+# 开始训练网络
 history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
-# plot history
+# 画出损失图
 pyplot.plot(history.history['loss'], label='train')
 pyplot.plot(history.history['val_loss'], label='test')
 pyplot.legend()
