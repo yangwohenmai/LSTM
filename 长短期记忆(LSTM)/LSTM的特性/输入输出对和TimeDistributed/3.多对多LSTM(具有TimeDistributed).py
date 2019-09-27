72bb170cfc2174333ac9181633baf058de8aa9af
yangwohenmai <zongxianyang@outlook.com>
2019-09-27 11:26:35 +0800

    update

--- 长短期记忆(LSTM)/LSTM的特性/输入输出对和TimeDistributed/3.多对多LSTM(具有TimeDistributed).py
+++ 长短期记忆(LSTM)/LSTM的特性/输入输出对和TimeDistributed/3.多对多LSTM(具有TimeDistributed).py
@@ -4,9 +4,10 @@ from keras.layers import Dense
 from keras.layers import TimeDistributed
 from keras.layers import LSTM
 """
-TimeDistributed这个封装器将一个层应用于输入的每个时间片,输入至少为 3D，且第一个维度应该是时间所表示的维度。
-如：input_shape=(32,10,16)考虑32个样本的一个batch，其中每个样本是10个16 维向量的序列
+TimeDistributed这个封装器将一个层应用于输入的每个时间片,输入至少为 3D，且第一个维度应该是时间所表示的维度。(维度为0,1,2)
+如：input_shape=(32,10,16)考虑32个样本的一个batch，其中每个样本是10个16 维向量的序列，其中10指的是时间步
 使用 TimeDistributed 来将 Dense 层独立地应用到 这 10 个时间步的每一个
+这里面10指的是时间步，这就是时间分布层TimeDistributed名字的由来，他使得每一个时间步都调用一次某个相同的网络层，调用的这些网络层，都有相同的权重参数
 """
 # prepare sequence
 length = 5
