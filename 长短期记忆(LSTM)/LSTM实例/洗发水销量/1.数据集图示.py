e0214ec7ffccb2832e9646cefa83a536b3407860
yangwohenmai <zongxianyang@outlook.com>
2019-10-09 15:13:53 +0800

    洗发水销量

---
+++ 长短期记忆(LSTM)/LSTM实例/洗发水销量/1.数据集图示.py
@@ -0,0 +1,13 @@
+# load and plot dataset
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot
+# 加载数据
+def parser(x):
+	return datetime.strptime('190'+x, '%Y-%m')
+series = read_csv('shampoo.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+# 显示开头部分行
+print(series.head())
+# 画图
+series.plot()
+pyplot.show()
