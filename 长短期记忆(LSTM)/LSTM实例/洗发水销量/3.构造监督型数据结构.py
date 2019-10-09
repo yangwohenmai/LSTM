0eed965b9140ef90be5412cde94f1da523f01877
yangwohenmai <zongxianyang@outlook.com>
2019-10-09 18:48:55 +0800

    洗发水销量

---
+++ 长短期记忆(LSTM)/LSTM实例/洗发水销量/3.构造监督型数据结构.py
@@ -0,0 +1,32 @@
+from pandas import read_csv
+from pandas import datetime
+from pandas import DataFrame
+from pandas import concat
+
+# 构造监督学习数据结构
+def timeseries_to_supervised(data, lag=1):
+    df = DataFrame(data)
+    print(df)
+    # 该行生成一个数组，将df向下移动一位作为第一个元素
+    columns = [df.shift(i) for i in range(1, lag+1)]
+    print(columns)
+    print(df)
+    # df作为第二个元素
+    columns.append(df)
+    # 将df.shift(1)，df按列合并，构成监督型数据
+    df = concat(columns, axis=1)
+    print(df)
+    # 将NaN位置补零
+    df.fillna(0, inplace=True)
+    return df
+
+
+# load dataset
+def parser(x):
+    return datetime.strptime('190'+x, '%Y-%m')
+series = read_csv('shampoo-sales.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+# transform to supervised learning
+X = series.values
+print(X)
+supervised = timeseries_to_supervised(X, 1)
+print(supervised)
