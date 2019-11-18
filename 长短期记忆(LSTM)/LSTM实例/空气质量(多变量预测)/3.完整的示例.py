84237a7f61f4052f4564676992fd460f15d0549d
yangwohenmai <zongxianyang@outlook.com>
2019-11-18 17:15:24 +0800

    多变量预测

--- 长短期记忆(LSTM)/LSTM实例/空气质量(多变量预测)/3.完整的示例.py
+++ 长短期记忆(LSTM)/LSTM实例/空气质量(多变量预测)/3.完整的示例.py
@@ -10,44 +10,56 @@ from sklearn.metrics import mean_squared_error
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+import pandas as pd
+
+pd.set_option('display.max_columns',1000)
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_colwidth',1000)
+
  
 # convert series to supervised learning
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
-	n_vars = 1 if type(data) is list else data.shape[1]
-	df = DataFrame(data)
-	cols, names = list(), list()
-	# input sequence (t-n, ... t-1)
-	for i in range(n_in, 0, -1):
-		cols.append(df.shift(i))
-		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
-	# forecast sequence (t, t+1, ... t+n)
-	for i in range(0, n_out):
-		cols.append(df.shift(-i))
-		if i == 0:
-			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
-		else:
-			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
-	# put it all together
-	agg = concat(cols, axis=1)
-	agg.columns = names
-	# drop rows with NaN values
-	if dropnan:
-		agg.dropna(inplace=True)
-	return agg
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = DataFrame(data)
+    print(df)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        print(cols)
+        names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        print(cols)
+        if i == 0:
+        	names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+        else:
+        	names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+    # put it all together
+    agg = concat(cols, axis=1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan:
+    	agg.dropna(inplace=True)
+    return agg
 
 # load dataset
 dataset = read_csv('pollution.csv', header=0, index_col=0)
 values = dataset.values
-# integer encode direction
+print(values)
+# 对风向进行数字编码转换
 encoder = LabelEncoder()
 values[:,4] = encoder.fit_transform(values[:,4])
-# ensure all data is float
+print(values[:,4])
+# 数据转换为浮点型
 values = values.astype('float32')
-# normalize features
+# 将所有数据放缩到（0，1）之间
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled = scaler.fit_transform(values)
 # frame as supervised learning
 reframed = series_to_supervised(scaled, 1, 1)
+print(reframed)
 # drop columns we don't want to predict
 reframed.drop(reframed.columns[[9,10,11,12,13,14,15]], axis=1, inplace=True)
 print(reframed.head())
