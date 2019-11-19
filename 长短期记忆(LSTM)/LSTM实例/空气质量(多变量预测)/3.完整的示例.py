f3972c4dda7347b401e97d794e56578834855dca
yangwohenmai <zongxianyang@outlook.com>
2019-11-19 17:14:22 +0800

    多变量预测

--- 长短期记忆(LSTM)/LSTM实例/空气质量(多变量预测)/3.完整的示例.py
+++ 长短期记忆(LSTM)/LSTM实例/空气质量(多变量预测)/3.完整的示例.py
@@ -37,8 +37,11 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
         else:
         	names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
     # put it all together
+    print(cols)
     agg = concat(cols, axis=1)
+    print(agg)
     agg.columns = names
+    print(agg)
     # drop rows with NaN values
     if dropnan:
     	agg.dropna(inplace=True)
@@ -48,53 +51,58 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
 dataset = read_csv('pollution.csv', header=0, index_col=0)
 values = dataset.values
 print(values)
-# 对风向进行数字编码转换
+# 对第四列“风向”进行数字编码转换
 encoder = LabelEncoder()
 values[:,4] = encoder.fit_transform(values[:,4])
 print(values[:,4])
 # 数据转换为浮点型
 values = values.astype('float32')
-# 将所有数据放缩到（0，1）之间
+# 将所有数据缩放到（0，1）之间
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled = scaler.fit_transform(values)
-# frame as supervised learning
+# 将数据格式化成监督学习型数据
 reframed = series_to_supervised(scaled, 1, 1)
 print(reframed)
-# drop columns we don't want to predict
+# 删掉那些我们不想预测的列
 reframed.drop(reframed.columns[[9,10,11,12,13,14,15]], axis=1, inplace=True)
 print(reframed.head())
  
 # split into train and test sets
 values = reframed.values
+print(values)
+# 取出一年的数据作为训练数据，剩下的做测试数据
 n_train_hours = 365 * 24
 train = values[:n_train_hours, :]
 test = values[n_train_hours:, :]
-# split into input and outputs
+# 将数据分割成输入和输出，最后一列数据作为输出数据
 train_X, train_y = train[:, :-1], train[:, -1]
 test_X, test_y = test[:, :-1], test[:, -1]
-# reshape input to be 3D [samples, timesteps, features]
+# 将输入数据转换成3D张量 [samples, timesteps, features]
 train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
 test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
 print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
  
-# design network
+# 设计网络结构
 model = Sequential()
 model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
 model.add(Dense(1))
 model.compile(loss='mae', optimizer='adam')
-# fit network
+# 拟合网络
 history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
-# plot history
+# 图像展示训练损失
 pyplot.plot(history.history['loss'], label='train')
 pyplot.plot(history.history['val_loss'], label='test')
 pyplot.legend()
 pyplot.show()
  
-# make a prediction
+# 使用拟合后的网络进行预测
 yhat = model.predict(test_X)
+print(yhat)
 test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
+print(test_X)
 # invert scaling for forecast
 inv_yhat = concatenate((yhat, test_X[:, 1:]), axis=1)
+# 对预测数据逆缩放
 inv_yhat = scaler.inverse_transform(inv_yhat)
 inv_yhat = inv_yhat[:,0]
 # invert scaling for actual
@@ -102,6 +110,6 @@ test_y = test_y.reshape((len(test_y), 1))
 inv_y = concatenate((test_y, test_X[:, 1:]), axis=1)
 inv_y = scaler.inverse_transform(inv_y)
 inv_y = inv_y[:,0]
-# calculate RMSE
+# 计算RMSE值
 rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
 print('Test RMSE: %.3f' % rmse)
