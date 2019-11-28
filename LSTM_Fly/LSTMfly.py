6de167a7359b1e4660b0ab5cd73ec06474b53d01
yangwohenmai <zongxianyang@outlook.com>
2019-11-28 15:05:28 +0800

    航班预测

--- LSTM_Fly/LSTMfly.py
+++ LSTM_Fly/LSTMfly.py
@@ -7,8 +7,7 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
-#%matplotlib inline
-
+#matplotlib inline
 
 # load the dataset
 dataframe = read_csv('international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
@@ -20,14 +19,15 @@ plt.plot(dataset)
 plt.show()
 
 
-# X is the number of passengers at a given time (t) and Y is the number of passengers at the next time (t + 1).
-
-# convert an array of values into a dataset matrix
+# X是给定时间(t)的乘客人数，Y是下一次(t + 1)的乘客人数。
+# 将值数组转换为数据集矩阵,look_back是步长。
 def create_dataset(dataset, look_back=1):
     dataX, dataY = [], []
     for i in range(len(dataset)-look_back-1):
         a = dataset[i:(i+look_back), 0]
+        # X按照顺序取值
         dataX.append(a)
+        # Y向后移动一位取值
         dataY.append(dataset[i + look_back, 0])
     return numpy.array(dataX), numpy.array(dataY)
 
@@ -35,24 +35,24 @@ def create_dataset(dataset, look_back=1):
 numpy.random.seed(7)
 
 
-# normalize the dataset
+# 数据缩放
 scaler = MinMaxScaler(feature_range=(0, 1))
 dataset = scaler.fit_transform(dataset)
 
 
-# split into train and test sets
+# 将数据拆分成训练和测试，2/3作为训练数据
 train_size = int(len(dataset) * 0.67)
 test_size = len(dataset) - train_size
 train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
 
 
-# use this function to prepare the train and test datasets for modeling
+# 构建监督学习型数据
 look_back = 1
 trainX, trainY = create_dataset(train, look_back)
 testX, testY = create_dataset(test, look_back)
 
 
-# reshape input to be [samples, time steps, features]
+# 数据重构为3D [samples, time steps, features]
 trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
 testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
 
@@ -65,17 +65,17 @@ model.compile(loss='mean_squared_error', optimizer='adam')
 model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
 
 
-# make predictions
+# 开始预测
 trainPredict = model.predict(trainX)
 testPredict = model.predict(testX)
 
-# invert predictions
+# 逆缩放预测值
 trainPredict = scaler.inverse_transform(trainPredict)
 trainY = scaler.inverse_transform([trainY])
 testPredict = scaler.inverse_transform(testPredict)
 testY = scaler.inverse_transform([testY])
 
-
+# 你算误差
 trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
 print('Train Score: %.2f RMSE' % (trainScore))
 testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
