f6e1865ddceb1546f17acc955a0b4d077f32b4f1
yangwohenmai <zongxianyang@outlook.com>
2019-11-29 17:04:48 +0800

    航班预测

--- LSTM_Fly/2.使用窗口方法进行回归(3→1).py
+++ LSTM_Fly/2.使用窗口方法进行回归(3→1).py
@@ -8,7 +8,11 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+import pandas as pd
 """
+用三列数据预测一列数据
+变动部分：
+look_back = 3
 X1	X2	X3	Y
 112	118	132	129
 118	132	129	121
@@ -16,6 +20,10 @@ X1	X2	X3	Y
 129	121	135	148
 121	135	148	148
 """
+pd.set_option('display.max_columns',1000)
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_colwidth',1000)
+
 # convert an array of values into a dataset matrix
 def create_dataset(dataset, look_back=1):
 	dataX, dataY = [], []
@@ -30,28 +38,29 @@ numpy.random.seed(7)
 dataframe = read_csv('airline-passengers.csv', usecols=[1], engine='python')
 dataset = dataframe.values
 dataset = dataset.astype('float32')
-# normalize the dataset
+# 缩放数据
 scaler = MinMaxScaler(feature_range=(0, 1))
 dataset = scaler.fit_transform(dataset)
-# split into train and test sets
+# 分割2/3数据作为测试
 train_size = int(len(dataset) * 0.67)
 test_size = len(dataset) - train_size
 train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-# reshape into X=t and Y=t+1
+# 预测数据步长为3,三个预测一个，3->1
 look_back = 3
 trainX, trainY = create_dataset(train, look_back)
 testX, testY = create_dataset(test, look_back)
-# reshape input to be [samples, time steps, features]
+# reshape 输入数据 [samples, time steps, features]
 trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
 testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
-# create and fit the LSTM network
+# 构建 LSTM 网络
 model = Sequential()
 model.add(LSTM(4, input_shape=(1, look_back)))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
 model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
-# make predictions
+# 对训练数据的Y进行预测
 trainPredict = model.predict(trainX)
+# 对测试数据的Y进行预测
 testPredict = model.predict(testX)
 # invert predictions
 trainPredict = scaler.inverse_transform(trainPredict)
@@ -63,15 +72,21 @@ trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
 print('Train Score: %.2f RMSE' % (trainScore))
 testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
 print('Test Score: %.2f RMSE' % (testScore))
-# shift train predictions for plotting
+
+# 构造一个和dataset格式相同的数组，共145行，dataset为总数据集
 trainPredictPlot = numpy.empty_like(dataset)
+# 用nan填充数组
 trainPredictPlot[:, :] = numpy.nan
+# 将训练集预测的Y添加进数组，从第3位到第93+3位，共93行
 trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+
 # shift test predictions for plotting
 testPredictPlot = numpy.empty_like(dataset)
 testPredictPlot[:, :] = numpy.nan
+# 将测试集预测的Y添加进数组，从第94+4位到最后，共44行
 testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
-# plot baseline and predictions
+
+# 画图
 plt.plot(scaler.inverse_transform(dataset))
 plt.plot(trainPredictPlot)
 plt.plot(testPredictPlot)
