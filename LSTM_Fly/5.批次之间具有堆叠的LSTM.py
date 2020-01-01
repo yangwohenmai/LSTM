a764af8ae2a0c2bbeb473a5b10b1be44a0b61145
yangwohenmai <zongxianyang@outlook.com>
2020-01-02 00:00:09 +0800

    update

--- LSTM_Fly/5.批次之间具有堆叠的LSTM.py
+++ LSTM_Fly/5.批次之间具有堆叠的LSTM.py
@@ -45,7 +45,7 @@ testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 1))
 # 构建 LSTM 网络
 batch_size = 1
 model = Sequential()
-# 堆叠两层网络，return_sequences=True表示将上层的输入形状向下继续传递
+# 堆叠两层网络，参数return_sequences=True表示将上层的输入形状向下继续传递
 model.add(LSTM(4, batch_input_shape=(batch_size, look_back, 1), stateful=True, return_sequences=True))
 model.add(LSTM(4, batch_input_shape=(batch_size, look_back, 1), stateful=True))
 model.add(Dense(1))
