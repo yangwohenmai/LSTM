e81ac77539921189d9ab9b0ffa99d6f8b2544863
yangwohenmai <zongxianyang@outlook.com>
2020-08-17 17:33:57 +0800

    update

--- 长短期记忆(LSTM)/LSTM的特性/输入输出对和TimeDistributed/3.多对多LSTM(具有TimeDistributed).py
+++ 长短期记忆(LSTM)/LSTM的特性/输入输出对和TimeDistributed/3.多对多LSTM(具有TimeDistributed).py
@@ -4,10 +4,17 @@ from keras.layers import Dense
 from keras.layers import TimeDistributed
 from keras.layers import LSTM
 """
-TimeDistributed这个封装器将一个层应用于输入的每个时间片,输入至少为 3D，且第一个维度应该是时间所表示的维度。(维度为0,1,2)
-如：input_shape=(32,10,16)考虑32个样本的一个batch，其中每个样本是10个16 维向量的序列，其中10指的是时间步
-使用 TimeDistributed 来将 Dense 层独立地应用到 这 10 个时间步的每一个
-这里面10指的是时间步，这就是时间分布层TimeDistributed名字的由来，他使得每一个时间步都调用一次某个相同的网络层，调用的这些网络层，都有相同的权重参数
+在没有时间分布层TimeDistributed封装器的时候，如果想要输出一个形状为(1, 5, 1)：
+[[[0. ]
+  [0.2]
+  [0.4]
+  [0.6]
+  [0.8]]]
+的向量时，需要将这个向量“展平”成形状为(1, 5)的向量：[[0.  0.2 0.4 0.6 0.8]]，然后用Dense(5)来输出5个连续的值。这里(1, 5)其实忽略了“5个步长，每个步长一个特征值，即(1, 5, 1)”，这个特性，而单单输出了“一个序列，序列中包含5个值”
+
+在使用TimeDistributed封装器的时候，就不需要进行这种“展平”操作，直接使用TimeDistributed(Dense(1))，就可以输出一个(1, 5, 1)的数据。
+
+使用TimeDistributed来将Dense层独立地应用到这5个时间步的每一个，这就是时间分布层TimeDistributed名字的由来，他使得每一个时间步都调用一次某个相同的网络层，调用的这些网络层，都有相同的权重参数
 """
 # prepare sequence
 length = 5
